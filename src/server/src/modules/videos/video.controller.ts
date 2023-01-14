a5a05eb03a9de13c867148b61e4f04d40e2212e6
luke-h1 <luke.howsam@yahoo.com>
2023-01-14 22:18:22 +0000

    feat(server): finish streaming endpoint

--- src/server/src/modules/videos/video.controller.ts
+++ src/server/src/modules/videos/video.controller.ts
@@ -1,7 +1,6 @@
 import busboy from 'busboy';
 import { Request, Response } from 'express';
 import fs from 'fs';
-import { prisma } from '../../db/prisma';
 import { UpdateVideoParams, UpdateVideoSchema } from './video.schema';
 import {
   createVideo,
@@ -28,6 +27,7 @@ export async function uploadVideoHandler(req: Request, res: Response) {
     owner: user.id,
   });
 
+  // eslint-disable-next-line consistent-return
   bb.on('file', async (_, file, info) => {
     const { mimeType } = info;
 
@@ -105,5 +105,73 @@ export async function findVideosHandler(_req: Request, res: Response) {
 
   return res.status(200).json({
     videos,
+  });
+}
+
+// eslint-disable-next-line consistent-return
+export async function streamVideoHandler(
+  req: Request<{ videoId: string }>,
+  res: Response,
+) {
+  const { videoId } = req.params;
+
+  const { range } = req.headers;
+
+  // tell server what part of the document should be returned (in order to know what chunk to send)
+
+  if (!range) {
+    return res.status(400).json({
+      message: 'Requires Range header',
+    });
+  }
+
+  const video = await findVideo(videoId);
+
+  if (!video) {
+    return res.status(404).json({
+      message: 'Video not found',
+    });
+  }
+
+  const filePath = getPath({
+    videoId,
+    extension: video.extension,
+  });
+
+  const fileSizeInBytes = fs.statSync(filePath).size;
+
+  const chunkStart = Number(range.replace(/\D/g, ''));
+
+  const chunkEnd = Math.min(
+    chunkStart + CHUNK_SIZE_IN_BYTES,
+    fileSizeInBytes - 1,
+  );
+
+  const contentLength = chunkEnd - chunkStart + 1;
+
+  const headers = {
+    'Content-Range': `bytes ${chunkStart}-${chunkEnd}/${fileSizeInBytes}`,
+    'Accept-Ranges': 'bytes',
+    'Content-length': contentLength,
+    'Content-Type': `video/${video.extension}`,
+    'Cross-Origin_Resource-Policy': 'cross-origin', // tell browser to allow cross origin requests (for video streaming)
+  } as const;
+
+  // tell the browser that the server is sending a partial response
+  res.writeHead(206, headers);
+
+  // create read stream for the video file
+  // only load this chunk into memory instead of the whole file
+  const videoStream = fs.createReadStream(filePath, {
+    start: chunkStart,
+    end: chunkEnd,
+  });
+
+  // pipe the video stream to the response
+  videoStream.pipe(res);
+
+  // when the video stream is done, end the response
+  videoStream.on('end', () => {
+    res.end();
   });
 }
